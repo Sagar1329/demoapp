527738607fd3318062a1eeab4e65ba01e944e453
Sagar <93616394+Sagar1329@users.noreply.github.com>
2023-07-10 10:01:42 +0530

    Completed the ui before 2nd video

--- src/App.js
+++ src/App.js
@@ -1,46 +1,40 @@
 import ItemDate from './components/ItemDate';
 import './App.css';
-import Item from './components/Item'
-import Card from './components/Card';
+
+import Products from './components/Products';
 function App() {
-  const response = [{
-    itemName: "Nirma",
-    itemDate: "20",
-    itemMonth: "July",
-    itemyear: "!990"
-  }, {
-    itemName: "Nirma2",
-    itemDate: "201",
-    itemMonth: "Jul222y",
-    itemyear: "!9920"
-
-
-  }, {
-    itemName: "Nirm33a",
-    itemDate: "203",
-    itemMonth: "Ju3ly",
-    itemyear: "!9903"
-
-
-  }
-
-
+  const products = [
+    {
+      id: 'p1',
+      title: 'Nirma',
+      amount: 100,
+      date: new Date(2021, 8, 10)
+    },
+    {
+      id: 'p2',
+      title: 'Niqrma',
+      amount: 130,
+      date: new Date(2022, 8, 10)
+    },
+    {
+      id: 'p3',
+      title: 'Nirwefsema',
+      amount: 1040,
+      date: new Date(2024, 8, 10)
+    },
+    {
+      id: 'p4',
+      title: 'Nirmaggg',
+      amount: 1002,
+      date: new Date(2023, 8, 10)
+    },
 
 
   ]
   return (
-    <Card><div><Item name={response[0].itemName}>Hemanth is chutiya</Item>
-      <ItemDate day={response[0].itemDate} month={response[0].itemMonth} year={response[0].itemyear}></ItemDate>
-
-      <Item name={response[1].itemName}></Item>
-      <ItemDate day={response[1].itemDate} month={response[1].itemMonth} year={response[1].itemyear}></ItemDate>
-
-
-      <Item name={response[2].itemName}></Item>
-      <ItemDate day={response[2].itemDate} month={response[2].itemMonth} year={response[2].itemyear}></ItemDate>
-      <div className="App">
-        Fuck you
-      </div></div></Card>
+    <div>
+      < Products item={products} />
+    </div>
 
 
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,13 @@
 
 import './Card.css';
-function Card(props) {
+const Card = (props) => {
+    const classes = 'card' + props.className;
     return (
-        <div className="Card">  {props.children}</div>
+        <div >
+            <div className='Div1'></div>
+            <div className={classes}>  {props.children}</div>
+            <div className='Div2'></div></div>
+
     );
 }
 export default Card;
