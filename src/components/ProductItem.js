ac44b373ffbab398aafa341a4ca6cbc3e01ce946
Sagar <93616394+Sagar1329@users.noreply.github.com>
2023-07-10 12:56:52 +0530

    flex wrap

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -10,15 +10,15 @@ const ProductItem = (props) => {
     const [title, setTitle] = useState(props.title);
 
     function clickHandler() {
-        setTitle("Porpcorn")
+        setTitle("Porpcorn");
         console.log("Button clicked")
     }
     return (<Card className='product-item'>
 
-        <ProductDate date={props.date} />
+        <ProductDate date={ props.date } />
         <div className="product-item_description">
-            <h2>{title}</h2>
-            < button className="CartButton" onClick={clickHandler}>Add to cart</button>
+            <h2>{ title }</h2>
+            < button className="CartButton" onClick={ clickHandler }>Add to cart</button>
 
         </div>
 
